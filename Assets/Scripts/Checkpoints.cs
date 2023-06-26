c94e008fe2731b7e872e8a2e6bf94d983e763cd4
Innominate-Dev <InnominateDev@gmail.com>
2023-06-26 11:50:35 +0100

    Added New Font
    - Updating New checkpoint system

--- Assets/Scripts/Checkpoints.cs
+++ Assets/Scripts/Checkpoints.cs
@@ -1,18 +1,32 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using TMPro;
 
 public class Checkpoints : MonoBehaviour
 {
+    public int checkpointsCompleted;
+    public TextMeshProUGUI checkpoints;
+    public List <GameObject> Checkpoint;
     // Start is called before the first frame update
     void Start()
     {
-        
+        //checkpointsCompleted = Checkpoint
     }
 
     // Update is called once per frame
     void Update()
     {
         
+    }
+
+    private void OnTriggerEnter(Collider other)
+    {
+        if (other.tag == "Player")
+        {
+            checkpointsCompleted = checkpointsCompleted - 1;
+            checkpoints.text = (checkpointsCompleted + " Left");
+            Destroy(gameObject);
+        }
     }
 }
