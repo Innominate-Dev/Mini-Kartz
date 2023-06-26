399a01fd595817cd2ef3cc623c2cd80bac5a948d
Innominate-Dev <InnominateDev@gmail.com>
2023-06-27 00:19:51 +0100

    Added Camera Animation
    - Added Camera Animation for the beginning of the game.
    - Added Round Manager and added states

---
+++ Assets/Scripts/RoundManager.cs
@@ -0,0 +1,30 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class RoundManager : MonoBehaviour
+{
+    public GameState state;
+    public enum GameState
+    {
+        Beginning,
+        InProgress,
+        Ending
+    }
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    void startingRound()
+    {
+
+    }
+}
